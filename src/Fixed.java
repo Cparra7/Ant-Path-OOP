9eeb57d95e38ebab56a9511d767e322d46954e09
Cristian Parra <108908482+Cparra7@users.noreply.github.com>
2025-09-13 19:08:49 -0700

    Create Fixed.java

---
+++ src/Fixed.java
@@ -0,0 +1,11 @@
+package com.mycompany.a1;
+
+import com.codename1.charts.models.Point;
+
+public abstract class Fixed extends GameObject {
+    public Fixed(int color, int size, Point location) {
+        super(color, size, location);
+    }
+    @Override
+    public void setLocation(float x, float y) { }
+}
